ef591bdea80e3f9808c4c241d9345b90485051ae
Nastya_Cipher <nastyakogy@gmail.com>
2019-12-26 02:00:33 +0300

    Добавил игнор и настрочил чутка кода
    Там ещё ничего не работает, так что можно не смотреть

---
+++ Hotel_Diamond/Hotel_Diamond/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Hotel_Diamond.Models;
+
+namespace Hotel_Diamond.Controllers
+{
+    public class HomeController : Controller
+    {
+        HotelContext db = new HotelContext();
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+       public ActionResult Autorization()
+       {
+            return View();
+       }
+
+        [HttpPost]
+        public ActionResult Autorization()
+        {
+            return "dsgr";
+        }
+    }
+}
---
+++ Hotel_Diamond/Hotel_Diamond/Models/HotelContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace Hotel_Diamond.Models
+{
+    public class HotelContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
